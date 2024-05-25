89926250cf4b7cdb3ec9a1e98809ed60aae7a6cb
Dheeraj P <dheerajp@Dheerajs-MacBook-Pro.local>
2024-05-26 04:19:02 +0530

    leftovers

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         "custom-gradient": "linear-gradient(to right, #fb7185, #ef4444)",
+        "purple-gradient": "linear-gradient(to right, #F600CE , #D81B60)",
+        "image": "url('../src/ui/images/sampleimg.jpg')",
       },
     },
     colors: {
@@ -13,6 +15,8 @@ module.exports = {
       lessblack: "#181818",
       lightred: "linear-gradient(to right, #fb7185, #ef4444)",
       lightgrey: "#576574",
+      red: "#e84118",
+      green: "#27ae60"
     },
   },
   plugins: [],
